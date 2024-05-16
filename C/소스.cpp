e6055706bd34f3b93efc865eb68f547e363c11a1
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-05-16 18:48:24 +0900

    TEST4

--- C/소스.cpp
+++ C/소스.cpp
@@ -2,7 +2,22 @@
 
 int main() {
 	
-	printf("Hello, World!!");
-	
+	int t;
+	int a;
+	scanf_s("%d", &t);
+	int min;
+	int max; 
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf_s("%d", &a);
+		if (max < a)
+			max = a;
+		if (min > a)
+			min = a;
+		
+	}
+
+	printf("%d,%d", min, max);
 	return 0;
 }
